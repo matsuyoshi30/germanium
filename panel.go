e5a66747d15ffbce88295ec898cdaf033df6ccae
Siavash Askari Nasr <ciavash@proton.me>
2022-06-14 20:14:54 +0430

    Update chroma to v2.2.0

--- panel.go
+++ panel.go
@@ -9,10 +9,10 @@ import (
 	"strings"
 	"unicode/utf8"
 
-	"github.com/alecthomas/chroma"
-	"github.com/alecthomas/chroma/formatters"
-	"github.com/alecthomas/chroma/lexers"
-	"github.com/alecthomas/chroma/styles"
+	"github.com/alecthomas/chroma/v2"
+	"github.com/alecthomas/chroma/v2/formatters"
+	"github.com/alecthomas/chroma/v2/lexers"
+	"github.com/alecthomas/chroma/v2/styles"
 	"golang.org/x/image/font"
 )
 
